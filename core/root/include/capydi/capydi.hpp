f426325506e2827ddf99bd3f1c763e0bca10dfd5
sakeof-capy <non-linear@ukr.net>
2026-03-23 17:48:19 +0100

    feat: capydi header containing everything

---
+++ core/root/include/capydi/capydi.hpp
@@ -0,0 +1,23 @@
+#ifndef CAPYDI_CAPYDI_HPP_
+#define CAPYDI_CAPYDI_HPP_
+
+/* Basic Includes */
+#include "Error.hpp"
+#include "Resolution.hpp"
+#include "ResolutionContext.hpp"
+#include "Container.hpp"
+
+/* Creational Configs */
+#include "configs/creational/Singleton.hpp"
+#include "configs/creational/Transient.hpp"
+
+/* Chainable Configs */
+#include "configs/chainable/Decorator.hpp"
+#include "configs/chainable/Observer.hpp"
+
+/* Decorative Configs */
+#include "configs/decorative/Interface.hpp"
+#include "configs/decorative/Tag.hpp"
+#include "configs/decorative/DependencyTags.hpp"
+
+#endif // !CAPYDI_CAPYDI_HPP_
